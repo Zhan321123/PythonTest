af8dec14168d86f44b614aa84730b64cfdf79d8b
zhan <2540220581@qq.com>
2024-07-02 23:12:39 +0800

    dataAnalysis

--- lib/matrix.py
+++ lib/matrix.py
@@ -1,5 +1,4 @@
 from typing import Sequence, Union
-
 import numpy as np
 
 
@@ -30,7 +29,7 @@ class MatrixBase:
         """矩阵求逆"""
         pass
 
-    def getCofactor(self) -> "MatrixBase":
+    def getCofactor(self, row: int, column: int) -> "MatrixBase":
         """矩阵余子式"""
         pass
 
@@ -53,13 +52,17 @@ class MatrixBase:
         """矩阵的迹"""
         pass
 
-    def getStep(self) -> "MatrixBase":
-        """化为阶梯型矩阵"""
+    def getUpStep(self) -> "MatrixBase":
+        """化为上三角阶梯型矩阵"""
         pass
 
-    def getMinimal(self) -> "MatrixBase":
-        """化为最简形矩阵"""
+    def getDownStep(self) -> "MatrixBase":
+        """化为下三角阶梯型矩阵"""
         pass
+
+    # def getMinimal(self) -> "MatrixBase":
+    #     """化为最简形矩阵"""
+    #     pass
 
     def isSymmetry(self) -> bool:
         """是否是对称矩阵"""
@@ -73,39 +76,55 @@ class MatrixBase:
         """矩阵是否可交换"""
         pass
 
-    def __imul__(self, other: Union["MatrixBase", float]):
-        """矩阵乘法，改变自身"""
-        pass
-
-    def __rmul__(self, other: "MatrixBase") -> "MatrixBase":
+    def __mul__(self, other: Union["MatrixBase", float]) -> "MatrixBase":
         """矩阵乘法，产生一个新的矩阵"""
 
-    def __iadd__(self, other: "MatrixBase"):
-        """矩阵加法，改变自身"""
-        pass
-
-    def __radd__(self, other: "MatrixBase") -> "MatrixBase":
+    def __add__(self, other: "MatrixBase") -> "MatrixBase":
         """矩阵加法，产生一个新的矩阵"""
 
     def solveLinearEquation(self):
         """线型方程组求解"""
         pass
 
+    def print(self):
+        """简洁打印，似乎没什么用"""
+        pass
+
+    def __str__(self):
+        return str(self.matrix)
+
 
 class Matrix(MatrixBase):
     def __init__(self, data: Sequence[Sequence]):
-        if not self.__isRectangle(data):
-            print("Matrix must be square")
         super().__init__(data)
+        self.__isRectangle(data)
 
-    def __isRectangle(self, data: Sequence[Sequence])->bool:
-        return all(len(i) == len(data) for i in data)
+    def __isRectangle(self, data: Sequence[Sequence]) -> bool:
+        if all(len(i) == len(data[0]) for i in data):
+            return True
+        else:
+            print("Matrix must be rectangle")
+            return False
 
     def __isSquare(self):
         if len(self.data) == len(self.data[0]):
             return True
         else:
             print(f"Matrix is not square, but got {self.getShape()} shape")
+            return False
+
+    def __checkRow(self, index: int) -> bool:
+        if len(self.data[index]) == len(self.data):
+            return True
+        else:
+            print(f"Row {index} is not a row, but got {len(self.data[index])} columns")
+            return False
+
+    def __checkCol(self, index: int) -> bool:
+        if len(self.data) == len(self.data[index]):
+            return True
+        else:
+            print(f"Column {index} is not a column, but got {len(self.data[index])} rows")
             return False
 
     def getShape(self) -> tuple[int, int]:
@@ -127,11 +146,69 @@ class Matrix(MatrixBase):
     def getRank(self) -> int:
         return np.linalg.matrix_rank(self.matrix)
 
-    def getCofactor(self) -> "MatrixBase":
-        pass
+    def getCofactor(self, row: int, column: int) -> "MatrixBase":
+        if self.__checkRow(row) and self.__checkCol(column):
+            return Matrix(np.delete(np.delete(self.matrix, row, 0), column, 1))
 
+    def getAdjoint(self) -> "MatrixBase":
+        if self.__isSquare():
+            if self.getValue() == 0:
+                raise ValueError("Matrix's value is 0, don't have adjoint matrix")
+            return Matrix(np.linalg.inv(self.matrix).T * self.getValue())
+
+    def getNorm(self) -> float:
+        return np.linalg.norm(self.matrix)
+
+    def getEigen(self) -> tuple:
+        return np.linalg.eig(self.matrix)
+
+    def getTrace(self) -> float:
+        return np.trace(self.matrix)
+
+    def getUpStep(self) -> "MatrixBase":
+        return Matrix(np.triu(self.matrix))
+
+    def getDownStep(self) -> "MatrixBase":
+        return Matrix(np.tril(self.matrix))
+
+    def isSymmetry(self) -> bool:
+        return np.allclose(self.matrix, self.matrix.T)
+
+    def isAntiSymmetry(self) -> bool:
+        return np.allclose(self.matrix, -self.matrix.T)
+
+    def isExchangeable(self, other: "MatrixBase") -> bool:
+        m = self.matrix * other.matrix
+        n = other.matrix * self.matrix
+        return np.allclose(m, n)
+
+    def __mul__(self, other: Union["MatrixBase", float]):
+        if isinstance(other, MatrixBase):
+            if self.getShape()[0] != other.getShape()[1]:
+                print(f"Matrix {self.getShape()} can't multiply {other.getShape()}")
+            else:
+                return self.matrix * other.matrix
+        else:
+            return self.matrix * other
+
+    def __add__(self, other: "MatrixBase") -> "MatrixBase":
+        if self.getShape() == other.getShape():
+            return self.matrix + other.matrix
+        else:
+            print(f"Matrix {self.getShape()} can't add {other.getShape()}")
+
+    def print(self):
+        print('---------matrix---------')
+        print(self.matrix)
+        print(f"shape = {self.getShape()}")
+        print(f'----------end-----------')
 
 
 if __name__ == '__main__':
-    m = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    print(m.getInverse())
+    m = Matrix([[1, 2, 3, 4], [2, 5, 6, 6], [3, 6, 10, 8]])
+    n = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 10], [9, 10, 11]])
+    # print(m.getInverse())
+    # print(m.getCofactor(1,2).matrix)
+    print(m * n)
+    print(n * m)
+    print(n * n)
