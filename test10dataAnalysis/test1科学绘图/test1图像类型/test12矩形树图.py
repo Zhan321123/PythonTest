6a602f3aaa84fe2f2a81b01bd9a09d93af3c988f
zhan <2540220581@qq.com>
2024-11-02 23:12:30 +0800

    data

--- test10dataAnalysis/test1科学绘图/test1图像类型/test12矩形树图.py
+++ test10dataAnalysis/test1科学绘图/test1图像类型/test12矩形树图.py
@@ -17,14 +17,14 @@ def rectTree(ax: plt.Axes, sizes: Sequence, labels: Sequence):
     """矩形树图"""
     sizes = np.array(sizes)
     squarify.plot(ax=ax, sizes=sizes, label=labels, alpha=0.7,
-                  color=matplotlib.colormaps['rainbow'](sizes / sizes.max()), )
+                  color=matplotlib.colormaps['rainbow'](sizes / sizes.max()), pad=True)
     ax.axis('off')  # 关闭坐标轴
     ax.set_title('矩形树图')
 
 
 if __name__ == '__main__':
     l = list('ABCDEFGHIJ')
-    s = np.arange(1, len(l)+1)
+    s = [1, 1, 3, 5, 8, 13, 21, 34, 55, 89]
 
     fig, axs = plt.subplots(1, 2)
     axs = axs.flatten()
--- test10dataAnalysis/test1科学绘图/test1图像类型/test14词云图.py
+++ test10dataAnalysis/test1科学绘图/test1图像类型/test14词云图.py
@@ -22,23 +22,26 @@ def wordOfText(ax: plt.Axes, ts: str):
 
 
 def wordOfFrequencies(ax: plt.Axes, pds: dict):
+    """通过词频创建词云图"""
     wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(pds)
     ax.imshow(wordcloud)
     ax.axis("off")
 
 
 if __name__ == '__main__':
-    text = """
+    text1 = """
     this is a test text, for every line, we can use it to test the word cloud. 
     You can use it to test the word cloud.
     But it is not easy to use.
     """
-    data = {'Python': 96, 'Java': 87, 'C++': 46, 'C#': 20, 'PHP': 10, 'Go': 17, 'Ruby': 1, 'Swift': 13,
-            'Scala': 11, 'Kotlin': 1, 'Rust': 1, }
+    text2 = {
+        'Python': 96, 'Java': 87, 'C++': 46, 'C#': 20, 'PHP': 10, 'Go': 17, 'Ruby': 1, 'Swift': 13,
+        'Scala': 11, 'Kotlin': 1, 'Rust': 1,
+    }
 
     fig, axs = plt.subplots(1, 2, figsize=(10, 5))
     axs = axs.flatten()
-    wordOfText(axs[0], text)
-    wordOfFrequencies(axs[1], data)
+    wordOfText(axs[0], text1)
+    wordOfFrequencies(axs[1], text2)
 
     plt.show()
