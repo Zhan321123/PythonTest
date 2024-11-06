cb0fdb680870304c4bc7ecc6a4900d4c0457899a
zhan <2540220581@qq.com>
2024-11-07 00:35:46 +0800

    data

---
+++ test6媒体/test1图像处理/test1拼接为gif.py
@@ -0,0 +1,41 @@
+"""
+将多张图片拼接为gif
+"""
+import os
+from typing import Union, Sequence
+
+from PIL import Image
+import numpy as np
+
+
+def imgsToGif(files: Sequence[str], savePath: str, duration: Union[int, Sequence[int]]):
+    """
+    将多张图片拼接为gif
+    gif的宽高与第一张一致
+    """
+    if isinstance(duration, (Sequence, np.ndarray)):
+        duration = list(duration)
+        if len(duration) != len(files):
+            print("duration序列长度与文件数量不一致")
+            return
+    try:
+        images = [Image.open(file) for file in files]
+        images[0].save(savePath,  # 保存路径
+                       save_all=True,  # 保存所有帧
+                       append_images=images[1:],  # 动画帧
+                       duration=duration,  # 帧间隔
+                       loop=0)  # 循环次数，0为无限循环
+        print("gif保存成功")
+    except FileNotFoundError as e:
+        print("没有找到文件", e)
+    except Exception as e:
+        print("发生其他错误", e)
+
+
+if __name__ == '__main__':
+    path = r'D:\code\pythonProject\PythonTest\test10dataAnalysis\test1科学绘图\file'
+    sp = rf"{os.environ['USERPROFILE']}\Desktop\output.gif"
+    fs = [fr"{path}\{i}.png" for i in range(1, 25)]
+    ds = np.arange(10, 10 + len(fs)) * 10
+    print(len(ds), len(fs))
+    imgsToGif(fs, sp, ds)
---
+++ test6媒体/test1图像处理/test3灰度图.py
@@ -0,0 +1,24 @@
+"""
+将图像转为灰度
+"""
+
+import os
+from typing import Union, Sequence
+from PIL import Image
+import numpy as np
+
+
+def toGray(file: str, savePath: str) -> None:
+    """
+    将图片转为灰度图
+    """
+    img = Image.open(file)
+    img = img.convert("L")
+    img.save(savePath)
+
+
+if __name__ == '__main__':
+    f = r"D:\code\pythonProject\PythonTest\test6媒体\file\1.jpeg"
+    out = rf"{os.environ['USERPROFILE']}\Desktop\output.jpg"  # 桌面路径
+
+    toGray(f, out)
